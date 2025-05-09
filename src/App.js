dbe9abbd693493495e4d40c43c176231133c7b3e
naylaaprn <naylaaprn@gmail.com>
2025-05-09 11:05:44 +0700

    rumah-makan

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { NavbarComponent } from './components';
-import { Menuu, Home, About, Review} from './pages';
+import { Menuu, Home, About, Review, Contact, Sukses} from './pages';
 
 export default class App extends Component {
   render() {
@@ -14,6 +14,8 @@ export default class App extends Component {
             <Route path="/menuu" element={<Menuu />} />
             <Route path="/about" element={<About />} />  {/* Pastikan path-nya sesuai */}
             <Route path="/review" element={<Review />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/sukses" element={<Sukses />} />
           </Routes>
         </main>
       </BrowserRouter>
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,5 +2,7 @@ import Menuu from './Menuu';
 import Home from './Home';
 import About from './About';
 import Review from './Review';
+import Contact from './Contact';
+import Sukses from './Sukses';
 
-export { Menuu, Home, About, Review };
+export { Menuu, Home, About, Review, Contact, Sukses };
